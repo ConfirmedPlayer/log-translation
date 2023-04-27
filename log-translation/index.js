cf33b97b4fc3c41566f3cf483356f86832a53e18
Confi <confirmedplayer@mail.ru>
2023-04-27 13:04:08 +0500

    minor improvements & comments

--- log-translation/index.js
+++ log-translation/index.js
@@ -5,14 +5,14 @@ const chokidar = require('chokidar');
 const os = require('os');
 const { execSync } = require('child_process');
 
-
+// Подключение конфига
 const configPath = path.join(__dirname, 'config.yml');
 const config = yaml.load(fs.readFileSync(configPath, 'utf8'));
 
-
+// Инициализация watcher для файлов с расширением .log
 const watcher = chokidar.watch(`${config.inputDirectory}/*.log`, { persistent: true });
 
-
+// systemd сервис
 const systemdService = `
 [Unit]
 Description=Log translation service
@@ -27,15 +27,15 @@ User=root
 WantedBy=multi-user.target
 `;
 
-
+// Функция, возвращающая username и pid по uid
 function getUserAndPidFromUid(uid)
 {
-  const { username, uid: userId } = os.userInfo(uid);
+  const { username } = os.userInfo(uid);
   const pid = process.pid;
-  return { username, userId, pid };
+  return { username, pid };
 }
 
-
+// Функция, следящая за изменениями в указанном каталоге
 function runWatcher()
 {
   watcher.on('change', (filePath) =>
@@ -55,16 +55,16 @@ function runWatcher()
 
     const outputFileName = `${path.basename(filePath)}.json`;
     const outputFilePath = path.join(config.outputDirectory, outputFileName);
-    const logDataJson = JSON.stringify(logData);
+    const logDataJson = JSON.stringify(logData); // Конвертация logData в JSON
 
-    fs.appendFileSync(outputFilePath, `${logDataJson}\n`);
+    fs.appendFileSync(outputFilePath, `${logDataJson}\n`); // Запись лога в JSON
   });
 }
 
-
+// Регистрация сервиса systemd
 function registerService()
 {
-  const path = '/etc/systemd/system/log-translation.service'
+  const path = '/etc/systemd/system/log-translation.service';
 
   if (!fs.existsSync(path))
   {
@@ -73,7 +73,7 @@ function registerService()
     execSync('sudo systemctl enable log-translation');
     execSync('sudo systemctl start log-translation');
   
-    console.log('Сервис зарегистрирован');
+    console.log('Сервис зарегистрирован\n');
   }
   console.log('Пожалуйста, закройте программу, если она запущена не как сервис systemd');
 }
